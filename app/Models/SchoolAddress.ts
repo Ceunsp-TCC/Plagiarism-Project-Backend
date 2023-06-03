0b7393030ecd46ac8b746e1615ad783e2fd0a68a
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-06-02 22:52:55 -0300

    feat(create models and migrations

---
+++ app/Models/SchoolAddress.ts
@@ -0,0 +1,35 @@
+import type { BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Schools from 'App/Models/Schools'
+
+export default class SchoolAddress extends BaseModel {
+  @column({ columnName: 'idSchool' })
+  public idSchool: number
+
+  @column()
+  public CEP: string
+
+  @column()
+  public street: string
+
+  @column()
+  public district: string
+
+  @column()
+  public city: string
+
+  @column()
+  public state: string
+
+  @column()
+  public complement: string
+
+  @column()
+  public number: number
+
+  @belongsTo(() => Schools, {
+    localKey: 'idSchool',
+    foreignKey: 'id',
+  })
+  public school: BelongsTo<typeof Schools>
+}
---
+++ database/factories/SchoolFactory.ts
@@ -0,0 +1,8 @@
+import Schools from 'App/Models/Schools'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Schools, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
