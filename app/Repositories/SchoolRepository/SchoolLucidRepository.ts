4e2115bf4d5f10eedd84331912bf22042f1d20cc
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-06-07 15:18:28 -0300

    feat(any tests school repository

--- app/Repositories/SchoolRepository/SchoolLucidRepository.ts
+++ app/Repositories/SchoolRepository/SchoolLucidRepository.ts
@@ -1,17 +1,17 @@
-import SchoolAddress from 'App/Models/SchoolAddress'
 import type SchoolRepositoryInterface from 'App/Interfaces/Repositories/SchoolRepositoryInterface'
 import Schools from 'App/Models/Schools'
+import type { SchoolDto, SchoolAddressDto } from 'App/Dtos/Schools/SchoolDto'
 
 export default class SchoolLucidRepository implements SchoolRepositoryInterface {
   constructor(private readonly model: typeof Schools) {
     this.model = model
   }
-  public async create(school: Schools, schoolAddress: SchoolAddress): Promise<Schools> {
+  public async create(school: SchoolDto, schoolAddress: SchoolAddressDto): Promise<boolean> {
     const create = await this.model.create(school)
 
     await create.related('address').create(schoolAddress)
 
-    return create
+    return true
   }
   public async findByEmail(email: string): Promise<Schools | null> {
     return await this.model.findBy('email', email)
