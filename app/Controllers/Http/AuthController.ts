45aab611f718d0fbdd28a28848cae5c4b9d9dd66
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-06-07 20:59:35 -0300

    feat(auth) endpoints logout,middleware and me

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,5 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import LoginService from 'App/Services/AuthServices/LoginService'
+import MeService from 'App/Services/AuthServices/MeService'
+import LogoutService from 'App/Services/AuthServices/LogoutService'
 import DefaultResponse from 'App/Utils/DefaultResponse'
 import HttpContext from '@ioc:Adonis/Core/HttpContext'
 import SchoolLucidRepository from 'App/Repositories/SchoolRepository/SchoolLucidRepository'
@@ -9,6 +11,8 @@ import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class AuthController {
   private loginService: LoginService
+  private meService: MeService
+  private logoutService: LogoutService
   constructor() {
     this.loginService = new LoginService(
       new DefaultResponse(),
@@ -16,10 +20,19 @@ export default class AuthController {
       new SchoolLucidRepository(Schools),
       Hash
     )
+    this.meService = new MeService(HttpContext, new DefaultResponse())
+    this.logoutService = new LogoutService(HttpContext, new DefaultResponse())
   }
   public async login({ request }: HttpContextContract) {
     const payload = await request.validate(LoginValidator)
 
     return await this.loginService.login(payload.email, payload.password)
+  }
+  public async me() {
+    return await this.meService.me()
+  }
+
+  public async logout() {
+    return await this.logoutService.logout()
   }
 }
---
+++ tests/functional/auth/logout.spec.ts
@@ -0,0 +1,45 @@
+import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+import SchoolFactory from 'Database/factories/SchoolFactory'
+
+const url = '/v1/auth/logout'
+const urlLogin = '/v1/auth/login'
+const urlMe = '/v1/auth/me'
+
+test.group('Logout', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('Should be is logout user', async ({ client }) => {
+    const school = await SchoolFactory.create()
+    await school.merge({ password: 'Alpha@12' }).save()
+
+    const login = await client.post(urlLogin).json({
+      email: school.email,
+      password: 'Alpha@12',
+      deviceName: 'browser',
+    })
+
+    const sut = await client.post(url).bearerToken(login.response.body.content.accessToken.token)
+    sut.assertStatus(200)
+    sut.assertBodyContains({ message: 'User successfully logged out' })
+  })
+  test('Should be verify is token turn invalid', async ({ client }) => {
+    const school = await SchoolFactory.create()
+    await school.merge({ password: 'Alpha@12' }).save()
+
+    const login = await client.post(urlLogin).json({
+      email: school.email,
+      password: 'Alpha@12',
+      deviceName: 'browser',
+    })
+
+    await client.post(url).bearerToken(login.response.body.content.accessToken.token)
+    const sut = await client.get(urlMe).bearerToken(login.response.body.content.accessToken.token)
+
+    sut.assertStatus(401)
+    sut.assertBodyContains({ message: 'Unauthorized' })
+  })
+})
