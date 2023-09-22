41651a25beb8406c147307664bdc54fdcc3d4130
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-09-21 23:45:48 -0300

    fix: types

--- database/factories/ActivityFactory.ts
+++ database/factories/ActivityFactory.ts
@@ -1,6 +1,7 @@
 import Activities from 'App/Models/Activities'
 import Factory from '@ioc:Adonis/Lucid/Factory'
 
+//@ts-ignore
 export default Factory.define(Activities, ({ faker }) => {
   return {
     //
